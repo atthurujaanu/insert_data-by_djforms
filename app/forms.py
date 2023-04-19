f23ccb11d7f979205740b5867c9a9e97cf9ac549
attthurjaanu <janur9862@gmail.com>
2023-04-19 18:56:48 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+
+class StudentForm(forms.Form):
+    sid=forms.IntegerField()
+    name=forms.CharField(max_length=100)
+    email=forms.EmailField()
+    def __str__(self):
+        return self.name
